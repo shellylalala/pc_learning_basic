00df1f4c839baaca61e69d5405c54e06949f955c
shellylalala <zhangxinlei0411@outlook.com>
2026-05-27 14:23:00 +0800

    feat: 新增计算机基础&一些配置调整

--- rspress.config.ts
+++ rspress.config.ts
@@ -6,13 +6,19 @@ export default defineConfig({
   base: "/pc_learning_basic/",
   root: path.join(__dirname, "docs"),
   title: "赛博藏经阁",
+  lang: "zh",
   icon: "/robot2.png",
   logo: {
     light: "/robot2.png",
     dark: "/robot2.png",
   },
+  logoText: "赛博藏经阁",
   themeConfig: {
     lastUpdated: true,
+    editLink: {
+      docRepoBaseUrl:
+        "https://github.com/shellylalala/pc_learning_basic/tree/master/docs",
+    },
     socialLinks: [
       {
         icon: "github",
